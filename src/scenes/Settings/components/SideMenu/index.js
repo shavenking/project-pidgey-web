3e3e9390c8f4dd8baf477e8911b015c3707b3284
shavenking <shavenking@gmail.com>
2017-04-01 09:58:43 +0800

    選單中移除未實作之功能

--- src/scenes/Settings/components/SideMenu/index.js
+++ src/scenes/Settings/components/SideMenu/index.js
@@ -11,9 +11,6 @@ export default class SideMenu extends Component {
                         <li className="nav-item">
                             <Link to="settings/work-dashboard" className="nav-link" activeClassName="active">工作項目</Link>
                         </li>
-                        <li className="nav-item">
-                            <Link to="settings/subcontractor-dashboard" className="nav-link disabled" activeClassName="active">協力廠商</Link>
-                        </li>
                     </ul>
                 </div>
             </div>
